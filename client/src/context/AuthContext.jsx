40b63bbdc1acec1c98af5ac244a9f562e97576f8
rambijjam <rb9097724@gmail.com>
2026-03-23 16:29:41 +0530

    handle token expiry in Auth Context

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,30 +1,59 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode"
 
 const AuthContext = createContext(null);
+
+const isTokenValid = (token)=>{
+    try{
+        const decoded = jwtDecode(token);
+        return decoded.exp*1000 > Date.now();
+    }catch{
+        return false;
+    }
+}
 
 export const AuthProvider = ({children})=>{
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    const logout = ()=>{
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        setUser(null);
+    }
+
     useEffect(()=>{
         const storedUser = localStorage.getItem('user')
         const token = localStorage.getItem('token')
-        if( storedUser && token){
+        if( storedUser && token && isTokenValid(token)){
             setUser(JSON.parse(storedUser));
+        }else{
+            logout();
         }
         setLoading(false);
     }, [])
+
+    useEffect(()=>{
+        const token = localStorage.getItem('token');
+        if(!token) return ;
+        try{
+            const decoded = jwtDecode(token);
+            const expiryTime = decoded.exp*1000 - Date.now();
+
+            if(expiryTime<=0) logout();
+            else{
+                const timer = setTimeout(logout, expiryTime);
+                return ()=>clearTimeout(timer);
+            }
+        }catch{
+            logout();
+        }
+    },[user]);
 
     const login = (token, userData)=>{
         localStorage.setItem('token', token);
         localStorage.setItem('user', JSON.stringify(userData));
         setUser(userData);
-    }
-
-    const logout = ()=>{
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-        setUser(null);
     }
 
     const isAuthenticated = !!user;
